ec8b1ece1184032aa601adbe23a7038e5f3d93d0
Oleksandr Liakhov <oleksandr.liakhov-ext@jci.com>
2026-02-03 17:46:16 +0700

    Some updated

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/seven-minute-workout/' : '/',
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? '/seven-minute-workout/' : '/',
   plugins: [react()],
-});
+}));
